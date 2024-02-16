9b954946175f5b224c5833b343e603d3833d6353
Nicholas Liem <nicholas.liem@shippit.com>
2024-02-16 20:00:47 +0700

    fix: root routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Web routes
-  get   'cryptool/tool', to: 'main#show', as: 'main_page'
-  root 'main#show'
+  root 'main#show', as: 'main_page'
 
   # File handling routes
+  post  'cryptool/encrypt', to: 'encrypted_files#encrypt', as: 'encrypt_text'
   post  'cryptool/upload', to: 'encrypted_files#upload', as: 'upload_encrypted_file'
   get   'cryptool/download/:id', to: 'encrypted_files#download', as: 'download_encrypted_file'
 end
