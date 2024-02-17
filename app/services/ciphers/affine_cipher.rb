47ca9331a06aae8f639de565883a8d05ec499c98
Gulilil <juan.csantoso@gmail.com>
2024-02-17 22:50:33 +0700

    fix: affine input and result function params

--- app/services/ciphers/affine_cipher.rb
+++ app/services/ciphers/affine_cipher.rb
@@ -12,7 +12,7 @@ module Ciphers
     end
 
     def encrypt_data(data, key)
-      key = key.gsub('-',' ')
+      key = key.gsub('-', ' ')
       keys = key.split
 
       return "Not Integer Keys Detected" unless Utils.is_integer(keys[0]) && Utils.is_integer(keys[1])
@@ -41,7 +41,7 @@ module Ciphers
     end
 
     def decrypt_data(data, key)
-      key = key.gsub('-',' ')
+      key = key.gsub('-', ' ')
       keys = key.split
 
       return "Not Integer Keys Detected" unless Utils.is_integer(keys[0]) && Utils.is_integer(keys[1])
