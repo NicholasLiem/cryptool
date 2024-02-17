d9786fbfdf6136e9aea073a0b67a182cdca5899e
Nicholas Liem <13521135@std.stei.itb.ac.id>
2024-02-17 09:15:18 +0700

    feat: scale spec file

--- spec/services/vigenere_cipher_spec.rb
+++ spec/services/vigenere_cipher_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe VigenereCipher do
-  let(:key) { 'KEY' }
-  let(:plaintext) { 'HELLOWORLD' }
-  let(:cipher_text) { 'RIJVSUYVJN' }
+  let(:cipher_alg) { VigenereCipher.new }
 
-  describe '#encrypt_data' do
-    it 'correctly encrypts plaintext' do
-      cipher = VigenereCipher.new
-      expect(cipher.encrypt_data(plaintext, key)).to eq(cipher_text)
-    end
-  end
-
-  describe '#decrypt_data' do
-    it 'correctly decrypts ciphertext' do
-      cipher = VigenereCipher.new
-      expect(cipher.decrypt_data(cipher_text, key)).to eq(plaintext)
+  describe '#encrypt_data and #decrypt_data' do
+    [
+      { key: 'KEY', plaintext: 'HELLOWORLD', cipher_text: 'RIJVSUYVJN' },
+      { key: 'DIFFERENTKEY', plaintext: 'ANOTHERTEXT', cipher_text: 'DVTYLVVGXHX' }
+    ].each do |test_case|
+      context "with key '#{test_case[:key]}'" do
+        it 'correctly encrypts and decrypts' do
+          encrypted = cipher_alg.encrypt_data(test_case[:plaintext], test_case[:key])
+          expect(encrypted).to eq(test_case[:cipher_text])
+          decrypted = cipher_alg.decrypt_data(encrypted, test_case[:key])
+          expect(decrypted).to eq(test_case[:plaintext])
+        end
+      end
     end
   end
 end
