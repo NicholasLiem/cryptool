0b879ed593e5fa32b582ab388e9310c6f3fb61f6
Gulilil <juan.csantoso@gmail.com>
2024-02-18 21:27:40 +0700

    feat: encryption for super encryption

--- test.rb
+++ test.rb
@@ -1,5 +1,38 @@
 TOTAL_ASCII = 256
 
+def transposition_cipher(row, col, text)
+  matrix = Array.new(row) { Array.new(col, ' ') }
+  remainder = row * col - text.length
+  (1..remainder).each do |i|
+    text += '*'
+  end
+
+  # Display Matrix
+  # (0..(text.length-1)).each do |i|
+  #   cur_row = i / row
+  #   cur_col =  i % col
+  #   matrix[cur_row][cur_col] = text[i]
+  # end
+  # matrix.each do |row|
+  #   puts row.join(' ')
+  # end
+
+
+  result = ''
+  (0..col-1).each do |j|
+    (0..row-1).each do |i|
+      temp_char = text[col*i + j]
+      result += temp_char
+    end
+  end
+
+  puts result
+
+
+  return matrix
+  
+end
+
 def encrypt_data(data, key)
   plain_text = data
   plain_text_length = plain_text.length
@@ -7,6 +40,8 @@ def encrypt_data(data, key)
 
   result = ""
   key_idx = 0
+
+  # Extended Vigenere
   (0..(plain_text_length - 1)).each do |i|
     # Reset if it reaches max
     key_idx = 0 if key_idx == key_length
@@ -27,7 +62,14 @@ def encrypt_data(data, key)
     # Increment
     key_idx += 1
   end
+
+  col = key.length
+  row = (result.length.to_f / col).ceil
+  transposition_cipher(row, col, result)
+
+
   result
 end
 
-puts(encrypt_data('B$%E$!%#A123##'.upcase, '%!&01#'))
+
+puts(encrypt_data('B$%E$!%#A123##34712BFCIE138ASAWE'.upcase, '%!&01#'))
